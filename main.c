000f93c83d613dc1371da96050f4476132601782
Rafael Brier <rafaelmjf2010@gmail.com>
2018-06-14 22:31:48 -0300

    working bluetooth

--- main.c
+++ main.c
@@ -21,7 +21,30 @@
 /* Board Header file */
 #include "Board.h"
 
+/*Libs*/
+#include <stdint.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdio.h>
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "driverlib/gpio.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/uart.h"
+#include "tm4c123gh6pm.h"
+
+#define redLED      (*((volatile uint32_t *)(0x42000000 + (0x400253FC-0x40000000)*32 + 1*4)))
+#define blueLED      (*((volatile uint32_t *)(0x42000000 + (0x400253FC-0x40000000)*32 + 2*4)))
+#define greenLED      (*((volatile uint32_t *)(0x42000000 + (0x400253FC-0x40000000)*32 + 3*4)))
+
+
 #define TASKSTACKSIZE   512
+
+void initUart1();
+void initLeds();
+void writeCharToUart1(char c);
+void writeStringToUart1(char* str);
 
 Task_Struct taskButtonStruct;
 Task_Struct taskCardStruct;
@@ -35,29 +58,37 @@ Char taskStack[TASKSTACKSIZE];
 //Char taskGetResponse[TASKSTACKSIZE];
 //Char taskValvula[TASKSTACKSIZE];
 
-/*
- *  ======== heartBeatFxn ========
- *  Toggle the Board_LED0. The Task_sleep is determined by arg0 which
- *  is configured for the heartBeat Task instance.
- */
 
-uint32_t i, ii;
+int index;
+char rxChar[10];
+
 Void taskButtonFunc(UArg arg0, UArg arg1)
 {
     while (1) {
-    for(i=0; i<=100; i++){
-    for(ii=0; ii<=100000; ii++){
+        while(UARTCharsAvail(UART1_BASE)) //loop while there are chars
+        {
+            rxChar[index] = UART1_DR_R;
+            if(rxChar[index-1]==13)
+            {
+                rxChar[index-1]='\0';
+                if(strcmp(rxChar,"red")==0)
+                    redLED^=1;
+                if(strcmp(rxChar,"blue")==0)
+                    blueLED^=1;
+                if(strcmp(rxChar,"green")==0)
+                    greenLED^=1;
+                index=0;
+            }
+            else {
+                index++;
+            }
+        }
     }
-    GPIO_toggle(Board_LED0);
 }
-    }}
 
 Void taskCardFunc(UArg arg0, UArg arg1)
 {
     while (1) {
-        Task_sleep(1000);
-         GPIO_toggle(Board_LED1);
-
     }
 }
 
@@ -87,20 +118,15 @@ Void taskValvulaFunc(UArg arg0, UArg arg1)
  */
 int main(void)
 {
-    Task_Params taskParams;
-
-    /* Call board init functions */
-    Board_initGeneral();
-    Board_initGPIO();
-    // Board_initI2C();
-    // Board_initSDSPI();
-    // Board_initSPI();
-    // Board_initUART();
-    // Board_initUSB(Board_USBDEVICE);
-    // Board_initWatchdog();
-    // Board_initWiFi();
+    /* Call board init functions *///Board_initGeneral();// Board_initGPIO(); // Board_initI2C();
+    // Board_initSDSPI();// Board_initSPI();// Board_initUART();// Board_initUSB(Board_USBDEVICE);
+    // Board_initWatchdog();// Board_initWiFi();
+    initLeds();
+    initUart1();
 
     /* Construct heartBeat Task  thread */
+    Task_Params taskParams;
+
     Task_Params_init(&taskParams);
     taskParams.arg0 = 1000;
     taskParams.stackSize = TASKSTACKSIZE;
@@ -114,10 +140,10 @@ int main(void)
 
 
     /* Turn on user LED */
-   // GPIO_write(Board_LED0, Board_LED_ON);
+    // GPIO_write(Board_LED0, Board_LED_ON);
 
     System_printf("Starting the example\nSystem provider is set to SysMin. "
-                  "Halt the target to view any SysMin contents in ROV.\n");
+            "Halt the target to view any SysMin contents in ROV.\n");
     /* SysMin will only print to the console when you call flush or exit */
     System_flush();
 
@@ -126,3 +152,38 @@ int main(void)
 
     return (0);
 }
+
+
+void initUart1(){
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_UART1); //habilita periférico UART0
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB); //habilita periférico GPIOA
+
+    GPIOPinConfigure(GPIO_PB0_U1RX); //Pin PA0 definido como RECEIVER
+    GPIOPinConfigure(GPIO_PB1_U1TX); //Pin PA1 definido como TRANSMITER
+    GPIOPinTypeUART(GPIO_PORTB_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+
+    UARTConfigSetExpClk(UART1_BASE, SysCtlClockGet(), 9600,
+                        (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE));
+
+}
+
+void writeCharToUart1(char c)
+{
+    while (UART1_FR_R & UART_FR_TXFF); //wait till Transmitter is not full
+    UART1_DR_R = c; //write to UART1
+}
+
+
+void writeStringToUart1(char* str)   //write a string to Uart1
+{
+    int i;
+    for (i = 0; i < strlen(str); i++)
+        writeCharToUart1(str[i]);
+}
+
+void initLeds(){
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3);
+
+}
+
